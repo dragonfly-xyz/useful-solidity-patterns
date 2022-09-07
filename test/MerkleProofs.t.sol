4f5d8da608574f6e9bd90396b98846be83928644
Lawrence Forman <me@merklejerk.com>
2022-09-07 18:17:44 -0400

    fix broken test fix foundry config

--- test/MerkleProofs.t.sol
+++ test/MerkleProofs.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
-import "../examples/merkle-proofs/MerkleProofs.sol";
+import "../patterns/merkle-proofs/MerkleProofs.sol";
 import "./TestUtils.sol";
 
 contract MerkleProofsTest is Test, TestUtils {
